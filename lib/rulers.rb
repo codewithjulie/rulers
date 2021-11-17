3fc9926eaa92ed0a1f24d2d10537767ed240548d
codewithjulie <codewithjulie@gmail.com>
2021-11-16 16:57:06 -0800

    add routing to rulers

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,14 +2,28 @@
 
 require_relative "rulers/version"
 require "rulers/array"
+require "rulers/routing"
 
 module Rulers
   class Error < StandardError; end
 
   class Application
     def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       `echo debug > debug.txt`;
-      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers! #{[1, 2, 3].silly}"]]
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
