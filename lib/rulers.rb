37fa4e524b7e8ecfeecbf7dc932f257fe9015da9
codewithjulie <codewithjulie@gmail.com>
2021-11-16 17:38:30 -0800

    add root route and 500 error

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -12,12 +12,20 @@ module Rulers
       if env['PATH_INFO'] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
       end
-      
+
+      if env['PATH_INFO'] == '/'
+        return [200, {'Content-Type' => 'text/html'}, ['Welcome to my homepage']]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
-      `echo debug > debug.txt`;
-      [200, {'Content-Type' => 'text/html'}, [text]]
+      begin
+        text = controller.send(act)
+        `echo debug > debug.txt`;
+        [200, {'Content-Type' => 'text/html'}, [text]]
+      rescue => exception
+        [500, {'Content-Type' => 'text/html'}, ['Whoopsie!']]
+      end
     end
   end
 
